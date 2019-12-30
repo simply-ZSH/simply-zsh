ba11a1f86815a5bd4cfda35cb34a05cff8aca3b4
Davide Garberi <dade.garberi@gmail.com>
2019-12-30 16:47:11 +0100

    Cut down functions

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -1,35 +1,9 @@
-autoload -Uz is-at-least
+# Show git operations without "dirtying" the shell
+export PAGER=less
+export LESS=-R
 
-# *-magic is known buggy in some versions; disable if so
-if [[ $DISABLE_MAGIC_FUNCTIONS != true ]]; then
-  for d in $fpath; do
-  	if [[ -e "$d/url-quote-magic" ]]; then
-  		if is-at-least 5.1; then
-  			autoload -Uz bracketed-paste-magic
-  			zle -N bracketed-paste bracketed-paste-magic
-  		fi
-  		autoload -Uz url-quote-magic
-  		zle -N self-insert url-quote-magic
-      break
-  	fi
-  done
-fi
+# Easy alias
+alias -- -='cd -'
 
-## jobs
-setopt long_list_jobs
-
-env_default 'PAGER' 'less'
-env_default 'LESS' '-R'
-
-## super user alias
-alias _='sudo '
-
-## more intelligent acking for ubuntu users
-if which ack-grep &> /dev/null; then
-  alias afind='ack-grep -il'
-else
-  alias afind='ack -il'
-fi
-
-# recognize comments
-setopt interactivecomments
+# grep alias
+alias grep='grep --color=auto --exclude-dir=.git'
--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -61,17 +61,8 @@ if [ -z "$ZSH_COMPDUMP" ]; then
   ZSH_COMPDUMP="${ZDOTDIR:-${HOME}}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
 fi
 
-if [[ $ZSH_DISABLE_COMPFIX != true ]]; then
-  source $ZSH/lib/compfix.zsh
-  # If completion insecurities exist, warn the user
-  handle_completion_insecurities
-  # Load only from secure directories
-  compinit -i -C -d "${ZSH_COMPDUMP}"
-else
-  # If the user wants it, load from all found directories
-  compinit -u -C -d "${ZSH_COMPDUMP}"
-fi
-
+# Load from all found directories
+compinit -u -C -d "${ZSH_COMPDUMP}"
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
