f32495939fb1b5e566142280467534ef35c69723
Davide Garberi <dade.garberi@gmail.com>
2022-09-21 10:27:36 +0200

    Don't use env_default
    * This is a function implemented in ohmyzsh that we got rid of
    * Use export instead which works just fine

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -1,6 +1,6 @@
 # Show git operations without "dirtying" the shell
-env_default 'PAGER' 'less'
-env_default 'LESS' '-R'
+export PAGER=less
+export LESS=-R
 
 # grep alias
 alias grep='grep --color=auto --exclude-dir=.git'
